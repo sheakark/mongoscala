9b7a863c1653c77f591461990d60d0d115d90a42
sheakar <skrishna@sungevity.com>
2015-08-02 20:10:42 -0700

    Commiting latest

--- DB.scala
+++ DB.scala
@@ -5,15 +5,15 @@ import scala.collection.convert.Wrappers._
 
 class DB private(val underlying:MongoDB) {
 
-	private def collection(name:String) = underlying.getCollection(name)
+	private def collection(name:String) = underlying.getCollection(name) 
 	
-	def readOnlyCollection(name: String) = new DBCollection(collection(name)) 
+	def readOnlyCollection(name: String) = new DBCollection(collection(name)) with Memoizer
 	//We are able to mixin administrable only because DBCollection also extends from ReadOnly. We cannot create
 	//a instance of DBcollection without providing the mongo collection which ReadOnly trait depends on.
 	//Adminstrable and Updatable trait extends ReadyOnly can only be mixed in with a class that also extends from ReadOnly
 	//since someone has to provide the dependency of ReadOnly before they can be mixed in
-	def administrableCollection(name: String) = new DBCollection(collection(name)) with Administrable
-	def updatableCollection(name: String) = new DBCollection(collection(name)) with Updatable
+	def administrableCollection(name: String) = new DBCollection(collection(name)) with Administrable with Memoizer
+	def updatableCollection(name: String) = new DBCollection(collection(name)) with Updatable with Memoizer
 
 	def collectionNames = for (name <- new 
 	    JSetWrapper(underlying.getCollectionNames) 
--- DBCollection.scala
+++ DBCollection.scala
@@ -1,7 +1,10 @@
 package com.scalainaction.mongo
 
+// Interface /Collection of traits that operate on a MongoDB "collection"
+
 import com.mongodb.{DBCollection => MongoDBCollection}
 import com.mongodb.DBObject
+import com.mongodb._
 
 class DBCollection(override val underlying:MongoDBCollection) extends ReadOnly
 
@@ -15,6 +18,8 @@ trait ReadOnly {
 	def getCount(doc:DBObject) = underlying getCount doc
 }
 
+//All traits extends readOnly since we have to find one before we operate on it 
+
 trait Administrable extends ReadOnly {
 	def drop:Unit = underlying drop
 	def dropIndexes = underlying dropIndexes
@@ -23,4 +28,25 @@ trait Administrable extends ReadOnly {
 trait Updatable extends ReadOnly {
 	def -=(doc: DBObject) = underlying remove doc
 	def +=(doc: DBObject) = underlying save doc
+}
+
+trait Memoizer extends ReadOnly {
+
+	val history = scala.collection.mutable.Map[Int, DBObject]()
+  val historyCursor = scala.collection.mutable.Map[Int, DBCursor]()
+
+	override def findOne = {
+    history.getOrElseUpdate(-1, {super.findOne})
+  }
+
+  override def findOne(doc:DBObject) = {
+    println("calling memoizer in findOne().........................")
+    history.getOrElseUpdate(doc.hashCode, {super.findOne(doc)})
+  }
+
+  override def find(doc:DBObject) = {
+    println("calling memoizer in find()..................")
+    historyCursor.getOrElseUpdate(doc.hashCode, {super.find(doc)})
+  }
+
 }
